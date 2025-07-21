7e2cf1f376a35abac23adebe72b9515b45cbe4d1
koren23 <79501333+koren23@users.noreply.github.com>
2025-07-21 09:41:23 +0300

    Update tx.vhd

--- tx.vhd
+++ tx.vhd
@@ -4,25 +4,25 @@ library IEEE;
     entity Transmiter is
         generic (
             stop_bit_length : integer := 1;  -- 1=1 baud, 2=1.5 baud, 3=2 baud
-            BaudRate : integer := 115200;
-            Clock_Freq : integer := 100e4
+            BaudRate        : integer := 115200;
+            Clk_Freq        : integer := 100e4
         );
         port (
-            clk  : in std_logic;
-            tx   : out std_logic;
-            flag : in std_logic; -- sent by rx to start
+            clk      : in std_logic;
+            tx       : out std_logic;
+            flag     : in std_logic; -- sent by rx to start
             databyte : in std_logic_vector(7 downto 0) -- data sent by rx
         );
     end Transmiter;
     
     architecture Behavioral of Transmiter is
         type statetype is (idle, start, data, stop);
-        signal state : statetype := idle;
-        signal counter : integer range 0 to 868 := 0;
-        signal sentflag : boolean := false;
-        signal stopcount : integer range 0 to 1 := 0;
-        signal Bit_counting_value : integer := (100*Clk_Freq)/BaudRate;
-        signal bit_number : integer range 0 to 7 := 0; -- location of bit being sent
+        signal state              : statetype              := idle;
+        signal counter            : integer range 0 to 868 := 0;
+        signal sentflag           : boolean                := false;
+        signal stopcount          : integer range 0 to 1   := 0;
+        signal Bit_counting_value : integer                := (100*Clk_Freq)/BaudRate;
+        signal bit_number         : integer range 0 to 7   := 0; -- location of bit being sent
     begin
         process(clk)
         begin
@@ -95,4 +95,4 @@ library IEEE;
                 end case;
             end if;
         end process;
-    end Behavioral;
+    end Behavioral;
