eb3bf3d010374b5e6235a477eb299475a4647cdc
koren23 <79501333+koren23@users.noreply.github.com>
2025-07-21 09:29:08 +0300

    Update rx.vhd

--- rx.vhd
+++ rx.vhd
@@ -3,24 +3,24 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity Receiver is
     generic (
         BaudRate : integer := 115200;
-        Clock_Freq : integer := 100e4
+        Clk_Freq : integer := 100e4
     );
     port(
-        clk : in  std_logic; -- clock coming from clock wizard (100MHZ)
-        rx  : in std_logic;-- rx coming from io0 external
+        clk           : in  std_logic; -- clock coming from clock wizard (100MHZ)
+        rx            : in std_logic;-- rx coming from io0 external
         output_vector : out std_logic_vector(7 downto 0);-- vector of 8 bits rx output
-        flag : out std_logic-- flag sent to tx means - start sending data
+        flag          : out std_logic-- flag sent to tx means - start sending data
         );
 end Receiver;
 
 architecture Behavioral of Receiver is
     type statetype is (idle, start, data, stop);
-    signal state : statetype := idle;
-	signal Bit_counting_value : integer := (100*Clk_Freq)/BaudRate;
-    signal counter : integer range 0 to 868 :=0; -- counting to 1 Bit in 115200 Baudrate 
-    signal tempdata  : std_logic_vector(7 downto 0) := (others => '0'); -- tempdata saving before releasing to tx
-	signal bit_number  : integer range 0 to 7 := 0; -- current rx to vector location
-	signal temporaryrx : std_logic;
+    signal state              : statetype                    := idle;
+	signal Bit_counting_value : integer                      := (100*Clk_Freq)/BaudRate;
+    signal counter            : integer range 0 to 868       :=0; -- counting to 1 Bit in 115200 Baudrate 
+    signal tempdata           : std_logic_vector(7 downto 0) := (others => '0'); -- tempdata saving before releasing to tx
+	signal bit_number         : integer range 0 to 7         := 0; -- current rx to vector location
+	signal temporaryrx        : std_logic;
 begin
     process(clk)
     begin
@@ -73,7 +73,8 @@ begin
                     else
                         counter <= counter + 1;
                     end if;
+                    
 			end case;
         end if;
     end process;
-end Behavioral;
+end Behavioral;
