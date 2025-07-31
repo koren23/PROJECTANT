bcbb9491f448fdd54b85ff44b49e30144b83a605
koren23 <79501333+koren23@users.noreply.github.com>
2025-07-31 12:38:45 +0300

    Update main.c

--- uartlite_led_control_with_log_message/main.c
+++ uartlite_led_control_with_log_message/main.c
@@ -1,68 +1,32 @@
 #include "xparameters.h"
 #include "xgpio.h"
-#include "xuartlite.h"
-#include "xuartlite_l.h"
-#include <stdio.h>
-#include <string.h>
+#include "xuartps.h"
+#include "xuartps_hw.h"
 
-int main()
+int main(void)
 {
-	XGpio ledgpio;
-	XUartLite uartlite;
+    XGpio led;
+    XUartPs uart;
+    XUartPs_Config *config;
 
-	XGpio_Initialize(&ledgpio,0); // initialize from xparameters.h
-	XGpio_SetDataDirection(&ledgpio, 1, 0x0); // (0 = output)
-	XUartLite_Initialize(&uartlite,0);
+    // initialize LED gpio
+    XGpio_Initialize(&led, 0    );
+    XGpio_SetDataDirection(&led, 1, 0x0); // set as output
+    XGpio_DiscreteWrite(&led, 1, 1);      // turn on LED
 
-	int ledstate = 0; //  0 for off 1 for on
-	char messages[][50] = {
-		"LED is already ON ",
-		"LED turned ON ",
-		"LED turned OFF ",
-		"LED is already OFF ",
-		"Invalid Command ",
-	};
+    // initialize UART
+    config = XUartPs_LookupConfig(0);
+    XUartPs_CfgInitialize(&uart, config, config->BaseAddress);
 
-	while(1){
-		int i;
-		if(!XUartLite_IsReceiveEmpty(0)){ // XUartLite_IsReceiveEmpt returns false when data is in FIFO . . . XPAR_UARTLITE_0_BASEADDR is the u32 BaseAddress
-			u8 byte = XUartLite_RecvByte(uartlite.RegBaseAddress); // saves data in 8 bits
-			switch(byte){
+    while(1){
+        // wait for incoming data
+        while (!XUartPs_IsReceiveData(config->BaseAddress));
 
-				case 0xA5:
-					XGpio_DiscreteWrite(&ledgpio, 1, 1); // turn led on
-					if(ledstate)
-						for(i = 0; i < strlen(messages[0]); i++){ // 17 chars in message
-							XUartLite_SendByte(uartlite.RegBaseAddress, messages[0][i]);
-						}
-					else
-						for(i = 0; i < strlen(messages[1]); i++){
-							XUartLite_SendByte(uartlite.RegBaseAddress, messages[1][i]);
-						}
-					ledstate = 1; // led is on
-					break;
+        // read the received byte
+        u8 received = XUartPs_RecvByte(config->BaseAddress);
 
-				case 0x33:
-					XGpio_DiscreteWrite(&ledgpio, 1, 0); // turn led off
-					if(ledstate)
-						for(i = 0; i < strlen(messages[2]); i++){
-							XUartLite_SendByte(uartlite.RegBaseAddress, messages[2][i]);
-						}
-					else
-						for(i = 0; i < strlen(messages[3]); i++){
-							XUartLite_SendByte(uartlite.RegBaseAddress, messages[3][i]);
-						}
-					ledstate = 0;
-					break;
+        // echo it back
+        XUartPs_Send(&uart, &received, 1);
+}
 
-				default:
-					for(i = 0; i < strlen(messages[4]); i++){
-							XUartLite_SendByte(uartlite.RegBaseAddress, messages[4][i]);
-						}
-			}
-
-		}
-
-	}
-	return 0;
 }
