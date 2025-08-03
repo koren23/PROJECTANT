f10ef6fc4b0caa870adc024fde9fef1c905c630e
koren23 <79501333+koren23@users.noreply.github.com>
2025-08-03 20:58:31 +0300

    Update controller.vhd

--- VHDL_VIVADO/BRAM_Storing_With_UART_In_PL/controller.vhd
+++ VHDL_VIVADO/BRAM_Storing_With_UART_In_PL/controller.vhd
@@ -11,11 +11,12 @@ use IEEE.STD_LOGIC_1164.ALL;
         end controller;
         
         architecture Behavioral of controller is
-        type actiontype is (write, read);
+        type actiontype is (write, read); 
+		type datastatetype is (startbyte,databyte,stoptestbyte,finalstopbyte);
         signal action        : actiontype := read;
-        signal tempdata      : std_logic_vector(7 downto 0) :="00000000";
-        signal tempaddr      : std_logic_vector(7 downto 0) :="00000000";
-        signal datastate     : integer range 0 to 2         :=0;
+        signal datastate     : datastatetype                :=startbyte;
+        signal tempdata      : std_logic_vector(7 downto 0) := (others => '0');
+        signal tempaddr      : std_logic_vector(7 downto 0) := (others => '0');
         signal prevdataready : std_logic                    :='0';
         signal signaldone    : std_logic                    :='0';
         signal counter       : integer range 0 to 2         :=0;
@@ -24,63 +25,67 @@ use IEEE.STD_LOGIC_1164.ALL;
             process(clk)
                 begin
                 if rising_edge(clk) then
-                     writestartout <= '0';
                      if(datareadyin = '1' and prevdataready = '0') then
+	-- 	rising edge of datareadyin coming from rx
                      
                          case datastate is
-                            when 0 =>
-                                action <= read;
-                                enaout <= '0';
+                            when startbyte => -- looking for *
+                                action <= read; -- default
+                                enaout <= '0'; -- disabling BRAM
                                 weaout <= '0';
-                                writestartout <= '0';  
                                 if(din = "00101010") then -- means *
-                                    datastate <= 1;
+                                    datastate <= databyte;
                                 end if;
-                            when 1 =>
-                                tempaddr <= din;
-                                datastate <= 2;
-                            when 2 =>
+                            when databyte => -- saving address
+                                tempaddr <= din; -- address 
+                                datastate <= stoptestbyte;
+                            when stoptestbyte => -- stop bit or continue 
                                 if(din = "00100011") then -- means #
-                                    datastate <= 0;
-                                    signaldone <= '1';
-                                else
+                                    datastate <= startbyte;
+                                    signaldone <= '1'; --start reading
+									action <= read;
+                                else -- data
                                     tempdata <= din;
-                                    action <= write;
+									datastate <= finalstopbyte;
+								end if;
+							when finalstopbyte =>
+								datastate <= startbyte; -- start state case all over again
+								if(din = "00100011") then -- means #
+									action <= write;
+									signaldone <= '1'; --start writing
                                 end if;
-                                signaldone <= '1';
                         end case;
                         
                      end if;
                      
-                     
-                     
-                     if(signaldone = '1') then
+                     if(signaldone = '1') then -- finished with state case
                         if(action = read) then
-                            startdelaytx <= '1';
-                            addressout <= tempaddr;
-                            dout <= dbramin;
+                            startdelaytx <= '1'; -- starts delay
+							enaout <= '1';-- general enable
+                            weaout <= '0'; -- disable write (reads)
+                            addressout <= tempaddr;	-- exports address to BRAM
+                            dout <= dbramin; -- exports data to tx
                         else -- write
-                                addressout <= tempaddr;
-                                dbramout <= tempdata;
+                                addressout <= tempaddr; -- exports address to BRAM
+                                dbramout <= tempdata; -- exports data to BRAM
                                 enaout <= '1'; -- general enable
                                 weaout <= '1'; -- enable write
                         end if;
-                        signaldone <= '0';
+                        signaldone <= '0'; -- resetting when done 
                      end if;
                      
-                     if(startdelaytx = '1') then
-                        if(counter = 2) then    
+                     if(startdelaytx = '1') then -- need to delay writestartout a little 
+                        if(counter = 2) then  -- delay for 2 clock counts (1 might work)  
                             writestartout <= '1'; -- tx starts writing
-                            enaout <= '1';
-                            weaout <= '0';
                             counter <= 0;
-                            startdelaytx <= '0';
+                            startdelaytx <= '0'; -- reset the entire if loop
                         else
                             counter <= counter + 1;
-                            startdelaytx <= '1';
                         end if;
+					 else
+						writestartout <= '0'; -- tx doesnt write until startdelaytx = '1'
                      end if;
-                     prevdataready <= datareadyin;
+                     prevdataready <= datareadyin; -- updating datareadyin to notice rising edge pattern
                  end if;
             end process;
         end Behavioral;
