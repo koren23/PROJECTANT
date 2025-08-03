16fce41c5d0e570f5b06486de49da01a572ef789
koren23 <79501333+koren23@users.noreply.github.com>
2025-08-03 16:41:51 +0300

    Update controller.vhd

--- VHDL_VIVADO/BRAM_Storing_With_UART_In_PL/controller.vhd
+++ VHDL_VIVADO/BRAM_Storing_With_UART_In_PL/controller.vhd
@@ -3,10 +3,10 @@ use IEEE.STD_LOGIC_1164.ALL;
 
         entity controller is
             Port (
-                clk, dataready : in std_logic;
-                din, douta           : in std_logic_vector(7 downto 0);
-                ena, wea, writestart : out std_logic;
-                address, dina, dout  : out std_logic_vector(7 downto 0)
+                clk, datareadyin : in std_logic;
+                din, dbramin           : in std_logic_vector(7 downto 0);
+                enaout, weaout, writestartout : out std_logic;
+                addressout, dbramout, dout  : out std_logic_vector(7 downto 0)
             );
         end controller;
         
@@ -24,14 +24,15 @@ use IEEE.STD_LOGIC_1164.ALL;
             process(clk)
                 begin
                 if rising_edge(clk) then
-                     writestart <= '0';
-                     if(dataready = '1' and prevdataready = '0') then
+                     writestartout <= '0';
+                     if(datareadyin = '1' and prevdataready = '0') then
+                     
                          case datastate is
                             when 0 =>
                                 action <= read;
-                                ena <= '0';
-                                wea <= '0';
-                                writestart <= '0';  
+                                enaout <= '0';
+                                weaout <= '0';
+                                writestartout <= '0';  
                                 if(din = "00101010") then -- means *
                                     datastate <= 1;
                                 end if;
@@ -48,32 +49,38 @@ use IEEE.STD_LOGIC_1164.ALL;
                                 end if;
                                 signaldone <= '1';
                         end case;
+                        
                      end if;
+                     
+                     
+                     
+                     if(signaldone = '1') then
+                        if(action = read) then
+                            startdelaytx <= '1';
+                            addressout <= tempaddr;
+                            dout <= dbramin;
+                        else -- write
+                                addressout <= tempaddr;
+                                dbramout <= tempdata;
+                                enaout <= '1'; -- general enable
+                                weaout <= '1'; -- enable write
+                        end if;
+                        signaldone <= '0';
+                     end if;
+                     
                      if(startdelaytx = '1') then
                         if(counter = 2) then    
-                            writestart <= '1'; -- tx starts writing
+                            writestartout <= '1'; -- tx starts writing
+                            enaout <= '1';
+                            weaout <= '0';
                             counter <= 0;
                             startdelaytx <= '0';
                         else
                             counter <= counter + 1;
-                        end if;
-                     end if;
-                     if(signaldone = '1') then
-                        if(action = read) then
                             startdelaytx <= '1';
-                            ena <= '1';
-                            wea <= '0';
-                            address <= tempaddr;
-                            dout <= douta;
-                        else -- write
-                                address <= tempaddr;
-                                dina <= tempdata;
-                                ena <= '1'; -- general enable
-                                wea <= '1'; -- enable write
                         end if;
-                        signaldone <= '0';
                      end if;
-                     prevdataready <= dataready;
+                     prevdataready <= datareadyin;
                  end if;
             end process;
         end Behavioral;
